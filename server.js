d3155f11435dda19d9c98065cb4be2dafa60044e
skanakia <skanakia1@gmail.com>
2018-06-08 14:00:32 -0400

    showing queue

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var http = require("http");
+// var http = require("http");
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
-var fs = require("fs")
+var fs = require("fs");
 
 var reservations = [];
 var waitingList = [];
@@ -11,65 +11,24 @@ var waitingList = [];
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-var server = http.createServer(handleRequest);
-
-function handleRequest(req, res) {
-    var path = req.url;
-
-    switch (path) {
-        case "/home":
-            return renderHomePage(req, res);
-        case "/tables":
-            return renderTablesPage(req, res);
-        case "/reserve":
-            return renderReservationPage(req, res);
-        default:
-            return renderHomePage(req, res);
-    }
-}
-
-function renderHomePage() {
-    fs.readFile(__dirname + "/index.html", function (err, data) {
-        if (err) {
-            res.writeHead(500, { "Content-Type": "text/html" });
-            res.end("<html><head><title>Oops</title></head><body><h1>Oops, there was an error</h1></html>");
-        }
-        else {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(data);
-        }
-    });
-}
-
-function renderReservationPage(req, res) {
-    fs.readFile(__dirname + "/reservation.html", function (err, data) {
-        if (err) {
-            res.writeHead(500, { "Content-Type": "text/html" });
-            res.end("<html><head><title>Oops</title></head><body><h1>Oops, there was an error</h1></html>");
-        }
-        else {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(data);
-        }
-    });
-}
-
-function renderTablesPage(req, res) {
-    fs.readFile(__dirname + "/tables.html", function (err, data) {
-        if (err) {
-            res.writeHead(500, { "Content-Type": "text/html" });
-            res.end("<html><head><title>Oops</title></head><body><h1>Oops, there was an error</h1></html>");
-        }
-        else {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(data);
-        }
-    });
-}
 
 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
 
+  app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reservation.html"));
+  });
 
+  app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+
+  app.get("/bookings", function(req, res) {
+    return res.json([reservations, waitingList]);
+  });
+  
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -81,8 +40,14 @@ function Reservation(name, phone, email, UniqueID) {
     id: UniqueID
 }
 
-app.post("/api/characters", function (req, res) {
-    var newcharacter = new Reservation(req.body);
+app.post("/bookings", function (req, res) {
+    var booking = req.body;
+
+    if (reservations.length < 5) {
+        reservations.push(booking);
+    } else {
+        waitingList.push(booking);
+    }
 
 
 });
